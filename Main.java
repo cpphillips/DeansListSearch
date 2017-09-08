3cc497963e5241abc7d959e839720511945b0bc6
Chris Phillips <cpphillips13@gmail.com>
2017-09-08 17:31:45 -0400

    Removed Clutter
    Removed unnecessary lines of code in the program

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-
 import javafx.application.Application;
 import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
@@ -23,19 +22,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	static ArrayList<String> names = new ArrayList<String>();
 	static ArrayList<Student> students = new ArrayList<Student>();
 	static String searchPerson = "";
-	static double starCount = 0, totalCount = 0;
+	static double totalCount = 0;
 	static boolean isNumber;
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		File in = new File("list.txt");
 		Scanner scan = new Scanner(in);
-		
+
 		String word;
-		
+
 		while(scan.hasNextLine())
 		{
 			word = scan.nextLine();
@@ -44,14 +42,14 @@ public class Main extends Application {
 		}
 		launch(args);
 	}
-	
+
 	public static void addStudent(String name)
 	{
 		boolean isPresList = false;
 		String[] splitNames = name.split(",");
 		String ln = splitNames[0];
 		String fn = splitNames[1].trim();
-		
+
 		if(fn.contains("*"))
 		{
 			isPresList = true;
@@ -62,78 +60,78 @@ public class Main extends Application {
 	}
 
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		GridPane pane = new GridPane();
-		
+
 		Label intro = new Label("Enter a Name to Search:");
 		TextField text = new TextField();
-		
+
 		Button searchButton = new Button("Search");
 		searchButton.setOnAction(new EventHandler<ActionEvent>() {
-			
+
             public void handle(ActionEvent event) {
-            	
+
             	retrieveText(text.getText());
                 GridPane namePane = search(searchPerson);
                 VBox secondaryLayout = new VBox();
-                
+
                 Label secondLabel = new Label("Name Results");
-                
+
                 ScrollPane scroll = new ScrollPane();
                 scroll.setPrefHeight(200);
-                
+
                 scroll.setContent(namePane);
-                
+
                 secondaryLayout.getChildren().add(secondLabel);
                 secondaryLayout.getChildren().add(scroll);
-                
+
                 secondaryLayout.setAlignment(Pos.TOP_CENTER);
-                
+
                 Scene secondScene = new Scene(secondaryLayout, 250, 250);
- 
+
                 Stage secondStage = new Stage();
                 secondStage.setScene(secondScene);
-                 
+
                 //Set position of second window, related to primary window.
                 secondStage.setX(primaryStage.getX() + 50);
                 secondStage.setY(primaryStage.getY());
-  
+
                 secondStage.show();
             }
         });
-		
+
 		pane.add(intro, 0, 0, 2, 1);
 		pane.add(text, 0, 1);
 		pane.add(searchButton, 1, 1);
-	    
+
 		//Formatting
 		pane.setPadding(new Insets(25, 30, 30, 30));
 		Scene scene = new Scene(pane, 300, 100);
 		primaryStage.setTitle("Dean's List");
 		primaryStage.setResizable(false);
-		
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}	
-	
+	}
+
 	private GridPane search(String term)
 	{
 		GridPane retPane = new GridPane();
-		
+
 		int matchCount = 0, errCount = 0;
 		String[] splits = term.split(" ");
 		String termFirstName = "", termLastName = "";
-		
+
 		termFirstName = splits[0];
 		if(splits.length > 1)
 			termLastName = splits[1];
-		
+
 		for(int i = 0; i < totalCount; i++)
 		{
 			if(!isNumber) {
 				if(splits.length > 1)
 				{
-					if((students.get(i).getFirstName().contains(termFirstName) || students.get(i).getLastName().contains(termFirstName)) 
+					if((students.get(i).getFirstName().contains(termFirstName) || students.get(i).getLastName().contains(termFirstName))
 						&& (students.get(i).getFirstName().contains(termLastName) || students.get(i).getLastName().contains(termLastName)))
 					{
 						if(students.get(i).getOnPresList()) {
@@ -143,7 +141,7 @@ public class Main extends Application {
 						else {
 							retPane.add(new Label(students.get(i).getLastName() + ", " + students.get(i).getFirstName()), 0, matchCount);
 							matchCount++;
-						}	
+						}
 					}
 				} else {
 					if((students.get(i).getFirstName().contains(termFirstName) || students.get(i).getLastName().contains(termFirstName)))
@@ -156,7 +154,7 @@ public class Main extends Application {
 							retPane.add(new Label(students.get(i).getLastName() + ", " + students.get(i).getFirstName()), 0, matchCount);
 							matchCount++;
 						}
-					}		
+					}
 				}
 			}
 			else {
@@ -168,7 +166,7 @@ public class Main extends Application {
 		}
 		return retPane;
 	}
-	
+
 	private boolean retrieveText(String message) {
 		try {
 			Integer.parseInt(message);
